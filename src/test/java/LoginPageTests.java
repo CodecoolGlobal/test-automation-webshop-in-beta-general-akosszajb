ef2abe3bb40d070974443dcf1b9d2ca8e8d0e0fa
Akos Szajbert <kisszajbert@gmail.com>
2024-09-18 12:25:50 +0200

    LoginPageTest-fixed-with-env

--- src/test/java/LoginPageTests.java
+++ src/test/java/LoginPageTests.java
@@ -19,47 +19,17 @@ import java.io.IOException;
 
 public class LoginPageTests {
     private String username;
-    private String password;
     private WebDriver driver;
     private WebDriverWait wait;
     private loginPage _loginPage;
     private WebElement productTitle;
     WebElement errorMessage;
+    private String passwordForAll;
+     
     
-    private String[] readCredentialsFromFile(int usernameLineNumber) throws IOException {
-        String username = null;
-        String password = null;
-
-
-        ClassLoader classLoader = getClass().getClassLoader();
-        try (InputStream inputStream = classLoader.getResourceAsStream("credentials.csv");
-             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
-
-            password = reader.readLine();
-            String line;
-            int currentLine = 1;
-
-            while ((line = reader.readLine()) != null) {
-                currentLine++;
-                if (currentLine == usernameLineNumber) {
-                    username = line;
-                    break;
-                }
-            }
-        }
-
-        return new String[]{username, password}; // Return the username and password
-    }
-
     @BeforeEach
     public void setup() throws IOException {
-        ClassLoader classLoader = getClass().getClassLoader();
-        try (InputStream inputStream = classLoader.getResourceAsStream("chromedriverpath.csv");
-             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
-            String chromeDriverPath = reader.readLine();
-            System.setProperty("webdriver.chrome.driver", chromeDriverPath);
-        }
-
+        passwordForAll = System.getenv("PW_FOR_ALL");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-search-engine-choice-screen");
         options.addArguments("--start-maximized");
@@ -67,15 +37,15 @@ public class LoginPageTests {
         driver = new ChromeDriver(options);
         wait = new WebDriverWait(driver, Duration.ofSeconds(3));
         _loginPage = new loginPage(driver);
+        
+        driver.get("http://www.saucedemo.com/");
     }
 
     @Test
     public void loginWithStandardUser() throws IOException {
-        String[] credentials = readCredentialsFromFile(2);  // 2nd line is standard_user
-        username = credentials[0];
-        password = credentials[1];
-
-        _loginPage.loginProcess(username, password);
+        username = System.getenv("STANDARD_USER");
+     
+        _loginPage.loginProcess(username, passwordForAll);
 
         productTitle = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@data-test='title' and text()='Products']")));
         Assertions.assertTrue(productTitle.isDisplayed(), "Products title is not displayed");
@@ -83,11 +53,9 @@ public class LoginPageTests {
 
     @Test
     public void loginWithLockedOutUser() throws IOException {
-        String[] credentials = readCredentialsFromFile(3);  // 3rd line is locked_out_user
-        username = credentials[0];
-        password = credentials[1];
-
-        _loginPage.loginProcess(username, password);
+        username = System.getenv("LOCKED_OUT_USER");
+        
+        _loginPage.loginProcess(username, passwordForAll);
 
         errorMessage = driver.findElement(By.xpath("//h3[@data-test='error']"));
         Assertions.assertTrue(errorMessage.isDisplayed(), "Error message is not displayed (Epic sadface: Sorry, this user has been locked out.)");
@@ -95,11 +63,9 @@ public class LoginPageTests {
 
     @Test
     public void loginWithProblemUser() throws IOException {
-        String[] credentials = readCredentialsFromFile(4);  // 4th line is problem_user
-        username = credentials[0];
-        password = credentials[1];
-
-        _loginPage.loginProcess(username, password);
+        username = System.getenv("PROBLEM_USER");
+        
+        _loginPage.loginProcess(username, passwordForAll);
 
         productTitle = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@data-test='title' and text()='Products']")));
         Assertions.assertTrue(productTitle.isDisplayed(), "Products title is not displayed");
@@ -107,23 +73,19 @@ public class LoginPageTests {
 
     @Test
     public void loginWithPerformanceGlitchUser() throws IOException {
-        String[] credentials = readCredentialsFromFile(5);  // 5th line is performance_glitch_user
-        username = credentials[0];
-        password = credentials[1];
-
-        _loginPage.loginProcess(username, password);
-
+        username = System.getenv("PERFORMANCE_GLITCH_USER");
+        
+        _loginPage.loginProcess(username, passwordForAll);
+        
         productTitle = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@data-test='title' and text()='Products']")));
         Assertions.assertTrue(productTitle.isDisplayed(), "Products title is not displayed");
     }
 
     @Test
     public void loginWithErrorUser() throws IOException {
-        String[] credentials = readCredentialsFromFile(6);  // 6th line is error_user
-        username = credentials[0];
-        password = credentials[1];
-
-        _loginPage.loginProcess(username, password);
+        username = System.getenv("ERROR_USER");
+        
+        _loginPage.loginProcess(username, passwordForAll);
 
         productTitle = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@data-test='title' and text()='Products']")));
         Assertions.assertTrue(productTitle.isDisplayed(), "Products title is not displayed");
@@ -131,11 +93,10 @@ public class LoginPageTests {
 
     @Test
     public void loginWithVisualUser() throws IOException {
-        String[] credentials = readCredentialsFromFile(7);  // 7th line is visual_user
-        username = credentials[0];
-        password = credentials[1];
-
-        _loginPage.loginProcess(username, password);
+        username = System.getenv("VISUAL_USER");
+        
+        _loginPage.loginProcess(username, passwordForAll);
+        
 
         productTitle = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@data-test='title' and text()='Products']")));
         Assertions.assertTrue(productTitle.isDisplayed(), "Products title is not displayed");
